e477424427f2294d0565c09e866a7dcf630f7acd
Martial Rahul Sharma <martialrahulsharma@gmail.com>
2023-12-02 22:18:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './component/AddTodo'
+import Todo from './component/Todo'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
